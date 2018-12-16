7877cee518fec9783465ffad348a4d52a8ae959a
1349636970 <a1349636970@gmail.com>
2018-12-16 13:05:32 -0800

    update

--- ShareWebsite/test.js
+++ ShareWebsite/test.js
@@ -1,5 +1,16 @@
-$.post('https://yingzheng.000webhostapp.com/', function (data) {
-    if (data) {
-        window.open(data)
+$("button").click(function() {
+    $.post('https://yingzheng.000webhostapp.com/', function (data) {
+        if (data) {
+            window.open(data)
+        }
+    })
+})
+
+$("input").keyup(function () {
+    var content = $("input").val()
+    content = content.trim()
+    if(event.keyCode == 13) {
+        $.get(`https://yingzheng.000webhostapp.com/?link=${content}`)
+        alert("request complete")
     }
 })
